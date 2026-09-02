5217ad48086d3635313400444e2e89889551a227
rana <rana.buddy@gmail.com>
2026-09-02 19:59:44 +0600

    added

---
+++ src/app/page.js
@@ -0,0 +1,6 @@
+import RubRankings from "@/components/RubRankings/RubRankings";
+
+export default function Home() {
+  return <RubRankings adminId="" posterId="" verifyId="" sitename={null} />;
+}
+
---
+++ src/config/index.js
@@ -0,0 +1,2 @@
+export const API_URL='https://meetbackend-three.vercel.app';
+export const SITE='rubrakings.vercel.app'
